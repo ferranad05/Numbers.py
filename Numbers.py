0cdf1ed3ccfcf909bc2b22b74dc561252bcda233
Ferran Álvarez <87202859+ferranad05@users.noreply.github.com>
2021-07-10 13:24:39 +0200

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,31 @@
+print(4+4) #Add
+print(5-4) #Subtract
+print(4*4) #Multiply
+print(10/2) #Divide
+print(10%3) #Give us the reminder from the operation, 10/3=3 with a reminder of 1
+
+#We can use () to specify the order from the operation, example:
+print(3*(4+5))
+
+########################################################################################################################
+my_num = 6
+print(str(my_num)+" is my favorite number") #Converts the number into a string
+########################################################################################################################
+my_num = -6
+print(abs(my_num)) #Give us the absolute value of the number
+########################################################################################################################
+print(pow(4, 2)) #Give us the result of multiplying a number a determinated number of times, in this situation: 4*4, because we have write to multiply 2 times
+########################################################################################################################
+print(max(4, 2)) #Tell us which number is higher
+########################################################################################################################
+print(min(4, 2)) #Tell us which number is lower
+########################################################################################################################
+print(round(4.6)) #Round a number
+########################################################################################################################
+from math import* #Import a math library with extra functions
+print(floor(3.7)) #Grab the lower number
+########################################################################################################################
+print(ceil(3.7)) #Will round the number up no matter what
+########################################################################################################################
+print(sqrt(36)) #Calculates the square root
+
